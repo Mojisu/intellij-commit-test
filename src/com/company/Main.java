4bc97abf0be5fd6adbcb6caba9cd95af5356ef70
Mojisu <modsbloom@gmail.com>
2016-09-27 21:30:54 +0900

    commit test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int i = 0;
+        int i = 1;
         System.out.println("commit test" + i);
     }
 }
