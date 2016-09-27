5233349b48a330f907a31332da069b1e4711e32c
Mojisu <modsbloom@gmail.com>
2016-09-28 02:18:31 +0900

    faster standard io

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,66 @@
 package com.company;
 
-public class Main {
+import java.io.*;
+import java.util.*;
 
+public class Main {
     public static void main(String[] args) {
-        int i = 1;
-        System.out.println("commit test" + i);
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        /*
+        int i = sc.nextInt();
+        long l = sc.nextLong();
+        double d = sc.nextDouble();
+        String str = sc.next();
+        String line = sc.nextLine();
+        */
+
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
