7546de8410ddd06b1ce66dd3b7fb5471e11bb30a
Mojisu <UlanS6nimda11!!U>
2016-09-29 23:03:43 +0900

    hollys

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,37 @@ public class Main {
         String str = sc.next();
         String line = sc.nextLine();
         */
+        for(int T = sc.nextInt(); T > 0; T--) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int dist = 0;
+            int min;
+
+            int V[] = new int[N];
+            int E[][] = new int[N][N];
+
+            for(int i = N; i > 0; i--) {
+                V[i] = -1;
+                for (int j = N; j > 0; j--)
+                    E[i][j] = -1;
+            }
+
+            for(int i = M; i > 0; i--)
+                E[sc.nextInt()][sc.nextInt()] = sc.nextInt();
+
+            int i = 0;
+            while(true) {
+                min = E[i][0];
+                for (int j = 1; j < N; j++) {
+                    if (E[i][j] > 0) {
+                        V[i] = j;
+                        if(E[i][j] < min)
+                            min = E[i][j];
+                    }
+                }
+            }
+
+        }
 
         out.close();
     }
