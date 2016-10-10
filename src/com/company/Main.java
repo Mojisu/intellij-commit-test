149ae4bff5d4fbc83e9d7de8b596f1b4073105a6
Mojisu <modsbloom@gmail.com>
2016-10-10 20:37:17 +0900

    dijkstra test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,102 +5,58 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        MyScanner sc = new MyScanner();
-        out = new PrintWriter(new BufferedOutputStream(System.out));
-
         /*
-        int i = sc.nextInt();
-        long l = sc.nextLong();
-        double d = sc.nextDouble();
-        String str = sc.next();
-        String line = sc.nextLine();
+        Scanner sc = null;
+        File file;
+        try {
+            file = new File("C:\\Users\\MO\\IdeaProjects\\intellij-commit-test\\src\\com\\company\\input");
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         */
-        for(int T = sc.nextInt(); T > 0; T--) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int T = sc.nextInt(); T > 0; T--) {
             int N = sc.nextInt();
             int M = sc.nextInt();
 
             int V[] = new int[N];
             int E[][] = new int[N][N];
 
-            for(int i = N; i > 0; i--) {
+            for (int i = N - 1; i >= 0; i--) {
                 V[i] = 0;
-                for (int j = N; j > 0; j--)
+                for (int j = N - 1; j >= 0; j--)
                     E[i][j] = -1;
             }
 
-            for(int i = M; i > 0; i--)
+            for (int i = M - 1; i >= 0; i--)
                 E[sc.nextInt()][sc.nextInt()] = sc.nextInt();
 
             int i = 0;
             int min = -1;
             int idx = -1;
 
-            while(true) {
-                /* update distance and minimum distance */
-                for(int j = 0; j < N; j++) {
-                    if(E[i][j] != -1 && (V[j] > V[i] + E[i][j] || V[j] == 0)) {
+            while (true) {
+                // update distance and minimum distance
+                for (int j = 0; j < N; j++) {
+                    if (E[i][j] != -1 && (V[j] > V[i] + E[i][j] || V[j] == 0)) {
                         V[j] = V[i] + E[i][j];
-                        if(min == -1 || min > V[j]) {
+                        if (min == -1 || min > V[j]) {
                             min = V[j];
                             idx = j;
                         }
                     }
                 }
-                if(idx == N-1)
+                if (idx == N - 1)
                     break;
                 else {
                     i = idx;
+                    min = -1;
                     continue;
                 }
             }
-            out.println(min);
-        }
-
-        out.close();
-    }
-
-    public static PrintWriter out;
-
-    public static class MyScanner {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public MyScanner() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            }
-            catch(IOException e){
-                e.printStackTrace();
-            }
-            return str;
+            System.out.println(min);
         }
     }
 }
